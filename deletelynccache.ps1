969a26ee33c7eb29564ff56e777bb05e9f767074
Sethfrid <oblivion_zero@hotmail.com>
2017-03-10 11:50:00 +0000

    Add files via upload

---
+++ deletelynccache.ps1
@@ -0,0 +1,5 @@
+﻿stop-process -ProcessName lync
+stop-process -ProcessName OUTLOOK
+remove-item -path $env:UserProfile\AppData\Local\Microsoft\Office\15.0\Lync\*.com -Recurse -Force
+Start-Process -FilePath "C:\Program Files (x86)\Microsoft Office\Office15\lync.exe"
+Start-Process -FilePath "C:\Program Files (x86)\Microsoft Office\Office14\outlook.exe"
