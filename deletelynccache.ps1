4ddfef4e6fcb0cb1918ab6c5d7862c182b5708ab
Sethfrid <oblivion_zero@hotmail.com>
2017-03-16 10:19:31 +0000

    Update deletelynccache.ps1

--- deletelynccache.ps1
+++ deletelynccache.ps1
@@ -3,3 +3,4 @@ stop-process -ProcessName OUTLOOK
 remove-item -path $env:UserProfile\AppData\Local\Microsoft\Office\15.0\Lync\*.com -Recurse -Force
 Start-Process -FilePath "C:\Program Files (x86)\Microsoft Office\Office15\lync.exe"
 Start-Process -FilePath "C:\Program Files (x86)\Microsoft Office\Office14\outlook.exe"
+#Start-Process -FilePath "C:\Program Files (x86)\Microsoft Office\Office15\outlook.exe"
