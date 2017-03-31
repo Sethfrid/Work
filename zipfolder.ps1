62f1fe328993e45323f758c01e5b8433c73c978a
Sethfrid <oblivion_zero@hotmail.com>
2017-03-31 16:03:53 +0100

    Add files via upload

---
+++ zipfolder.ps1
@@ -0,0 +1,20 @@
+﻿<#
+1.       Ability to take an input (UNC Path/Folder Name)
+2.       Get all the directories in the specified folder and choose the oldest 5
+3.       Use the command line version of winrar to zip the oldest 5 folders using the same name as the folder putting it in the same path
+
+#>
+
+$folder = 'c:\temp'
+
+$Data = (get-childitem $folder -directory | sort LastWriteTime -Descending | select-object -Last 5).Name
+
+ForEach ($file in $Data)
+
+{
+
+%{&$folder\RAR.exe a -r $folder\$file.rar $File}
+
+}
+
+ 
