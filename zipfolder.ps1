1f4cbc97715abe3b55db2d857c1554370fce0c6e
Sethfrid <oblivion_zero@hotmail.com>
2017-03-31 16:28:25 +0100

    Update zipfolder.ps1

--- zipfolder.ps1
+++ zipfolder.ps1
@@ -1,11 +1,6 @@
-﻿<#
-1.       Ability to take an input (UNC Path/Folder Name)
-2.       Get all the directories in the specified folder and choose the oldest 5
-3.       Use the command line version of winrar to zip the oldest 5 folders using the same name as the folder putting it in the same path
+#The rar.exe is needed
 
-#>
-
-$folder = 'c:\temp'
+$folder = read-host 'Enter folder location'
 
 $Data = (get-childitem $folder -directory | sort LastWriteTime -Descending | select-object -Last 5).Name
 
