009b544578c83b1d354225af56da1e31b863f185
Sethfrid <oblivion_zero@hotmail.com>
2017-04-20 12:34:52 +0100

    Update zipfolder.ps1

--- zipfolder.ps1
+++ zipfolder.ps1
@@ -1,5 +1,6 @@
-#The rar.exe is needed
-
+<#The rar.exe is needed in the same location as to where the archiving will take place
+ Takes toldest 5 folders and creates split volume archiving of 250MB  size.
+#>
 $folder = read-host 'Enter folder location'
 
 $Data = (get-childitem $folder -directory | sort LastWriteTime -Descending | select-object -Last 5).Name
@@ -8,7 +9,7 @@ ForEach ($file in $Data)
 
 {
 
-%{&$folder\RAR.exe a -r -ai $folder\$file.rar $File}
+%{&$folder\RAR.exe a -r -ai -v250m $folder\$file.rar $File}
 
 }
 
