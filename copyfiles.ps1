33941a54be7968ff5e411752ffdb42087d464fac
Sethfrid <oblivion_zero@hotmail.com>
2017-04-24 12:51:07 +0100

    Add files via upload

---
+++ copyfiles.ps1
@@ -0,0 +1,10 @@
+﻿<#
+Add a timestamp to each archive file and move them onto a backup location
+#>
+
+$dest = "\\ntanet1\salesforcebackup"
+$org = get-childitem "$env:USERPROFILE\downloads" -filter *.zip | ForEach-Object {          
+Rename-Item $_.FullName "$BackupFolder$($_.BaseName -replace " ", "_" -replace '\..*?$')_$(Get-Date -Format "ddMMyyyy").zip"
+}
+
+move-item "$env:userprofile\downloads\*.zip" -Destination $dest -recurse
