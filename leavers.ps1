780433e1eafa815899902a9ef70b74f6d5d4023b
Sethfrid <oblivion_zero@hotmail.com>
2017-08-08 13:25:52 +0100

    leavers in AD
    removes manager, removes all group memberships, disables account and set a date to expire.

---
+++ leavers.ps1
@@ -0,0 +1,8 @@
+﻿set-aduser samaccount -manager $null
+
+get-adprinciplegroupmembership samaccount | where {$_.Name -notlike "Domain Users"} |% {Remove-AdPrincipleGroupmembership samaccount -memberof $_ -Confirm:$false}
+
+disable-adaccount samaccount
+
+set-adaccountexpiration samaccount -DateTime "07/08/2017" 
+# In this date's example the account is set to expire the EOD of the previous day.
