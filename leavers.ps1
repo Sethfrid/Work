f64a363bdc4efdb8af08fae0d9e53eaea2559fcf
Sethfrid <oblivion_zero@hotmail.com>
2017-12-01 10:23:30 +0000

    Update leavers.ps1

--- leavers.ps1
+++ leavers.ps1
@@ -5,6 +5,10 @@ get-adprinciplegroupmembership samaccount | where {$_.Name -notlike "Domain User
 #Empties the mail, company and proxyaddresses fields
 set-aduser  samaccountname -replace @{mail="$null"; company ="$null"; proxyAddresses="$null"}
 
+remove-adgroupmember -identity sec-emsE5 -members SAMACCOUNTNAME -confirm:$false
+
+remove-adgroupmember -identity sec-o365e5 -members SAMACCOUNTNAME -confirm:$false
+
 disable-adaccount samaccountname
 
 set-adaccountexpiration samaccount -DateTime "07/08/2017" 
