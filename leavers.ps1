1a3ff430c541ea8db58463ce413775fdd7ce442b
Sethfrid <oblivion_zero@hotmail.com>
2017-12-01 09:48:14 +0000

    Update leavers.ps1

--- leavers.ps1
+++ leavers.ps1
@@ -1,12 +1,11 @@
-set-aduser samaccount -manager $null
- 
-set-aduser samaccount -email $null
-
-Set-ItemProperty -Filter "proxyAddresses=null"
+#removes all groups for a user
 
 get-adprinciplegroupmembership samaccount | where {$_.Name -notlike "Domain Users"} |% {Remove-AdPrincipleGroupmembership samaccount -memberof $_ -Confirm:$false}
 
-disable-adaccount samaccount
+#Empties the mail, company and proxyaddresses fields
+set-aduser  samaccountname -replace @{mail="$null"; company ="$null"; proxyAddresses="$null"}
+
+disable-adaccount samaccountname
 
 set-adaccountexpiration samaccount -DateTime "07/08/2017" 
 # In this date's example the account is set to expire the EOD of the previous day.
