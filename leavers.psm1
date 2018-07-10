87da51584771fd03af13ce5efe7df2f1be867944
Sethfrid <oblivion_zero@hotmail.com>
2018-07-10 12:51:15 +0100

    Update leavers.psm1

--- leavers.psm1
+++ leavers.psm1
@@ -1,23 +1,25 @@
 function disable-user {
 [cmdletbinding()]
 param(
-     [Parameter(Mandatory=$True)]
-          [string]$user
+     [Parameter(Mandatory=$True)]
+          [string]$user
 )
-
+ 
 set-aduser $user -clear company, mail, telephonenumber, ProxyAddresses, mobile
 disable-adaccount $user
-
+$person = get-aduser $user 
+move-adobject -identity $person.DistinguishedName -targetpath "OU=Disabled Users,OU=Managed Users,DC=domain,DC=org,DC=uk"
+ 
 }
-
-
+ 
+ 
 function remove-groups {
 [cmdletbinding()]
 param (
 [Parameter(Mandatory=$True)]
-          [string]$user
-
+          [string]$user
+ 
 )
 get-ADPrincipalGroupMembership $user | where {$_.Name -notlike "Domain Users"} |% {Remove-ADPrincipalGroupMembership $user -memberof $_ -Confirm:$false}
-
+ 
 }
