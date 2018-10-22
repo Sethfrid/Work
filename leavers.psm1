be662a4fa96d85e0f2f3683ae4cb4ab12e1c95f7
Sethfrid <oblivion_zero@hotmail.com>
2018-10-22 13:56:24 +0100

    Update leavers.psm1

--- leavers.psm1
+++ leavers.psm1
@@ -1,30 +1,30 @@
 function disable-user {
 [cmdletbinding()]
 param(
-     [Parameter(Mandatory=$True)]
-          [string]$user
+     [Parameter(Mandatory=$True)]
+          [string]$username
 )
- 
-set-aduser $user -clear company, mail, telephonenumber, ProxyAddresses, mobile
-disable-adaccount $user
-$person = get-aduser $user 
+
+set-aduser $username -clear company, mail, telephonenumber, ProxyAddresses, mobile
+disable-adaccount $username
+$person = get-aduser $username 
 move-adobject -identity $person.DistinguishedName -targetpath "OU=Disabled Users,OU=Managed Users,DC=domain,DC=org,DC=uk"
- 
+
 }
- 
- 
-function remove-groups {
+
+
+function remove-group {
 [cmdletbinding()]
 param (
 [Parameter(Mandatory=$True)]
-          [string]$user
- 
+          [string]$username
+
 )
 
-get-ADPrincipalGroupMembership $user | select name | export-csv c:\temp\$user.csv
+get-ADPrincipalGroupMembership $username | select name | export-csv c:\temp\$username.csv
 
 start-sleep -s 5
 
-get-ADPrincipalGroupMembership $user | where {$_.Name -notlike "Domain Users"} |% {Remove-ADPrincipalGroupMembership $user -memberof $_ -Confirm:$false}
- 
+get-ADPrincipalGroupMembership $username | where {$_.Name -notlike "Domain Users"} | foreach {Remove-ADPrincipalGroupMembership $username -memberof $_ -Confirm:$false}
+
 }
