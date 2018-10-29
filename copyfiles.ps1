46ca73138bf3bebbaf05b3ef96e0b378576602df
Sethfrid <oblivion_zero@hotmail.com>
2018-10-29 11:37:23 +0000

    Update copyfiles.ps1

--- copyfiles.ps1
+++ copyfiles.ps1
@@ -2,7 +2,7 @@
 Add a date stamp to each archive file and move them onto a backup location
 #>
 
-$dest = "\\ntanet1\salesforcebackup"
+$dest = "\\UNC\salesforcebackup"
 $org = get-childitem "$env:USERPROFILE\downloads" -filter *.zip | ForEach-Object {          
 Rename-Item $_.FullName "$BackupFolder$($_.BaseName -replace " ", "_" -replace '\..*?$')_$(Get-Date -Format "ddMMyyyy").zip"
 }
