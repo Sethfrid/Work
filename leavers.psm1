d70e34b5f27e3161500a71810cccf15705b793ac
Sethfrid <oblivion_zero@hotmail.com>
2020-08-26 12:18:11 +0100

    Update leavers.psm1

--- leavers.psm1
+++ leavers.psm1
@@ -1,46 +1,63 @@
-# This removes the attributes Company, Mail, Phone Number, Proxy Addresses and Mobile. 
-#The date/ time stamp along with the ticket number is added to the user under notes in the telephone tab of the user. 
+# This removes the attributes Company, Mail, Phone Number, Proxy Addresses and Mobile.
+
+#The date/ time stamp along with the ticket number is added to the user under notes in the telephone tab of the user.
+
 #Disables the account and moves to the Disabled Users OU
 
+ 
+
 function Disable-User {
+
 [cmdletbinding()]
+
 param(
+
      [Parameter(Mandatory=$True)]
+
      [string]$username,
 
+ 
+
      [Parameter(Mandatory=$True,
+
      HelpMessage = "Enter ticket number")]
+
      [string]$ticketnumber
-)
-
-
-
-$date = get-date
-
-set-aduser $username -clear company, mail, title, telephonenumber, mobile -replace @{description= "Account disabled on $date by $env:USERNAME (T# $ticketnumber )"}
-
-disable-adaccount $username
-
-$person = get-aduser $username 
-move-adobject -identity $person.DistinguishedName -targetpath "OU=Disabled Users,OU=Managed Users,DC=domain,DC=org,DC=uk"
-
-}
-
-
-#This exports all of the group members for a user then waits 5 seconds before removing them all except for Domain Users group, using the username as the file name under c:\temp
-
-function Remove-Group {
-[cmdletbinding()]
-param (
-[Parameter(Mandatory=$True)]
-          [string]$username
 
 )
 
-get-ADPrincipalGroupMembership $username | select name | export-csv "\\location\Leaver's Groups\$username.csv"
+ 
+
+get-ADPrincipalGroupMembership $username | select name | export-csv "\\##\##\IT\Leaver's Groups\$username.csv"
+
+ 
 
 start-sleep -s 5
 
-get-ADPrincipalGroupMembership $username | where {$_.Name -notlike "Domain Users"} | foreach {Remove-ADPrincipalGroupMembership $username -memberof $_ -Confirm:$false}
+ 
+
+get-ADPrincipalGroupMembership $username | where {$_.Name -notlike "Domain Users" -and $_.Name -notlike "sec-emsE5" -and $_.Name -notlike "sec-o365e5"} | foreach {Remove-ADPrincipalGroupMembership $username -memberof $_ -Confirm:$false}
+
+ 
+
+ 
+
+$date = get-date
+
+ 
+
+set-aduser $username -clear company, mail, title, telephonenumber, mobile -replace @{description= "Account disabled on $date by $env:USERNAME (T# $ticketnumber )"}
+
+ 
+
+disable-adaccount $username
+
+ 
+
+$person = get-aduser $username
+
+move-adobject -identity $person.DistinguishedName -targetpath "OU=Disabled Users,OU=Managed Users,DC=##,DC=##,DC=##"
+
+ 
 
 }
