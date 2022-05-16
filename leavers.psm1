1c62468e6bf09a8298ed5a09763f750396f0aa11
Sethfrid <oblivion_zero@hotmail.com>
2022-05-16 10:25:41 +0100

    Update leavers.psm1

--- leavers.psm1
+++ leavers.psm1
@@ -1,63 +1,31 @@
-# This removes the attributes Company, Mail, Phone Number, Proxy Addresses and Mobile.
-
-#The date/ time stamp along with the ticket number is added to the user under notes in the telephone tab of the user.
-
+# This removes the attributes Company, Mail, Phone Number, Proxy Addresses and Mobile. 
+#The date/ time stamp along with the ticket number is added to the user under notes in the telephone tab of the user. 
 #Disables the account and moves to the Disabled Users OU
 
- 
-
 function Disable-User {
-
 [cmdletbinding()]
-
 param(
-
      [Parameter(Mandatory=$True)]
-
      [string]$username,
 
- 
-
      [Parameter(Mandatory=$True,
-
      HelpMessage = "Enter ticket number")]
-
      [string]$ticketnumber
-
 )
 
- 
+get-ADPrincipalGroupMembership $username | select name | export-csv "\Leaver's Groups\$username.csv"
 
-get-ADPrincipalGroupMembership $username | select name | export-csv "\\##\##\IT\Leaver's Groups\$username.csv"
+start-sleep -s 3
 
- 
+get-ADPrincipalGroupMembership $username | where {$_.Name -notlike "Domain Users" -and $_.Name -notlike "License - Enterprise Mobility Security E5" -and $_.Name -notlike "License - Microsoft Office 365 E5"} | foreach {Remove-ADPrincipalGroupMembership $username -memberof $_ -Confirm:$false}
 
-start-sleep -s 5
-
- 
-
-get-ADPrincipalGroupMembership $username | where {$_.Name -notlike "Domain Users" -and $_.Name -notlike "sec-emsE5" -and $_.Name -notlike "sec-o365e5"} | foreach {Remove-ADPrincipalGroupMembership $username -memberof $_ -Confirm:$false}
-
- 
-
- 
 
 $date = get-date
 
- 
-
-set-aduser $username -clear company, mail, title, telephonenumber, mobile -replace @{description= "Account disabled on $date by $env:USERNAME (T# $ticketnumber )"}
-
- 
+set-aduser $username -clear company, department, mail, telephonenumber, mobile -replace @{description= "Account disabled on $date by $env:USERNAME (T# $ticketnumber )"}
 
 disable-adaccount $username
 
- 
+start-sleep -s 5
 
-$person = get-aduser $username
-
-move-adobject -identity $person.DistinguishedName -targetpath "OU=Disabled Users,OU=Managed Users,DC=##,DC=##,DC=##"
-
- 
-
-}
+Invoke-Command -ComputerName computer1 -ScriptBlock {sync}
