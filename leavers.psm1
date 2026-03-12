bb92c7f6c6f0a59a082725b47eda643be068ae71
Sethfrid <oblivion_zero@hotmail.com>
2026-03-12 11:32:11 +0000

    Update leavers.psm1

--- leavers.psm1
+++ leavers.psm1
@@ -1,7 +1,3 @@
-# This removes the attributes Company, Mail, Phone Number, Proxy Addresses and Mobile. 
-#The date/ time stamp along with the ticket number is added to the user under notes in the telephone tab of the user. 
-#Disables the account and moves to the Disabled Users OU
-
 function Disable-User {
 [cmdletbinding()]
 param(
@@ -13,15 +9,13 @@ param(
      [string]$ticketnumber
 )
 
-get-ADPrincipalGroupMembership $username | select name | export-csv "\Leaver's Groups\$username.csv"
+get-ADPrincipalGroupMembership $username | select name | export-csv "\\location\$username.csv"
 
-start-sleep -s 3
-
-get-ADPrincipalGroupMembership $username | where {$_.Name -notlike "Domain Users" -and $_.Name -notlike "License - Enterprise Mobility Security E5" -and $_.Name -notlike "License - Microsoft Office 365 E5"} | foreach {Remove-ADPrincipalGroupMembership $username -memberof $_ -Confirm:$false}
+get-ADPrincipalGroupMembership $username | where {$_.Name -notlike "Domain Users"} | foreach {Remove-ADPrincipalGroupMembership $username -memberof $_ -Confirm:$false}
 
 $date = get-date
 
-set-aduser $username -clear company, department, mail, telephonenumber, mobile -replace @{description= "Account disabled on $date by $env:USERNAME (T# $ticketnumber )"}
+set-aduser $username -clear company, mail, telephonenumber, mobile, manager -replace @{description= "Account disabled on $date by $env:USERNAME (T# $ticketnumber )"}
 
 Get-ADUser $username -Properties DirectReports | select -expandproperty DirectReports | Set-ADUser -clear manager
 
@@ -29,4 +23,8 @@ disable-adaccount $username
 
 start-sleep -s 5
 
-Invoke-Command -ComputerName computer1 -ScriptBlock {sync}
+Get-ADUser $username | Move-ADObject -TargetPath 'OU=Leavers,OU=Managed Users,DC=,DC=,DC='
+
+Invoke-Command -ComputerName core-aadc-01 -ScriptBlock {sync}
+
+}
